48b8f970fee3cd5448fa3e85eb4f00ea0410d538
Richard Robinson <rir@my.yorku.ca>
2018-12-24 08:50:53 -0500

    Added some JavaDoc

--- src/gpacalculator/eecs/Grade.java
+++ src/gpacalculator/eecs/Grade.java
@@ -4,6 +4,14 @@ abstract class Grade<T> implements Comparable<Grade> {
 	private T rawGrade;
 	private static String regex = "";
 
+	/**
+	 * The only constructor for the class. Only accepts valid grades of type String or type
+	 * Number, and throws an exception for all other cases
+	 *
+	 * @param rawGrade a valid String or Number
+	 * @throws ClassCastException       if rawGrade is not a String or a Number
+	 * @throws IllegalArgumentException if the grade is not a valid grade
+	 */
 	Grade(T rawGrade) {
 		if (!(rawGrade instanceof String || rawGrade instanceof Number))
 			throw new ClassCastException("Grade must be String or Number");
@@ -17,21 +25,52 @@ abstract class Grade<T> implements Comparable<Grade> {
 		this.rawGrade = rawGrade;
 	}
 
+	/**
+	 * Abstract method to map raw letter and percentage grades to a GPA scale. Implementations
+	 * of this method should typically call an overloaded static method depending on the type of
+	 * rawGrade (either String or Number)
+	 *
+	 * @return the scaled numeric GPA of the grade
+	 */
 	abstract double normalize();
 
+	/**
+	 * Sets the regex static class parameter, which is used by the constructor to check validity if
+	 * the parameter is a string, and throws an exception if necessary
+	 *
+	 * @param regex A character set of possible letter grades
+	 */
 	static void setRegex(String regex) {
 		Grade.regex = regex;
 	}
 
+	/**
+	 * Returns the raw grade of this instance
+	 *
+	 * @return the raw grade of this instance
+	 */
 	T getRawGrade() {
 		return this.rawGrade;
 	}
 
+	/**
+	 * Allows sorting of Grade objects by comparing the GPA normalized values of the two grades
+	 * via {@code Double.compare()}.
+	 *
+	 * @param g the Grade to be compared to
+	 * @return the return value described by {@code Double.compare()}
+	 * @see java.lang.Double
+	 */
 	@Override
 	public int compareTo(Grade g) {
 		return Double.compare(this.normalize(), g.normalize());
 	}
 
+	/**
+	 * Returns the normalized version of the grade as a string
+	 *
+	 * @return the normalized grade
+	 */
 	@Override
 	public String toString() {
 		return "" + normalize();
